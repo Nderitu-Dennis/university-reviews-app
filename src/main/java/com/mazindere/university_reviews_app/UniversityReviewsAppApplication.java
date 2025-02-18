f80dedc394995974eb08ad657bcab4584aa6b23f
Nderitu-Dennis <cit2270322021@mmu.ac.ke>
2025-02-18 16:26:40 +0300

    first commit

---
+++ src/main/java/com/mazindere/university_reviews_app/UniversityReviewsAppApplication.java
@@ -0,0 +1,13 @@
+package com.mazindere.university_reviews_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityReviewsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityReviewsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mazindere/university_reviews_app/UniversityReviewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazindere.university_reviews_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityReviewsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
