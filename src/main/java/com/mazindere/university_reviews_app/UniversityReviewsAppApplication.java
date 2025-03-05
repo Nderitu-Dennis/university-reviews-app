27aacbc4d54a798ac4274be307c0e3ec184e8440
Nderitu-Dennis <cit2270322021@mmu.ac.ke>
2025-03-05 11:29:29 +0300

    delete university-delay.js

--- src/main/java/com/mazindere/university_reviews_app/UniversityReviewsAppApplication.java
+++ src/main/java/com/mazindere/university_reviews_app/UniversityReviewsAppApplication.java
@@ -8,8 +8,6 @@ public class UniversityReviewsAppApplication {
 	//todo check on the user type -admin and user -to be checked later not now
 	//todo -shld navbar be also in sign up and log in pages?
 	//todo-make the hero section in university to delay-js not working
-	//todo -login error when user doesn't exist
-	//todo- registration success
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityReviewsAppApplication.class, args);
